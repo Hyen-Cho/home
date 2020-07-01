d53d94b4d4d5c29db38fe65ead9828f682c248fb
hyen <zltmalzosel@naver.com>
2020-07-01 14:02:54 +0900

    enhance img src

--- src/App.js
+++ src/App.js
@@ -103,67 +103,67 @@ class Technic extends Component {
             <ul>
               <li>
                 <div>
-                  <img src="/img/Java_logo.png" alt="1" />
+                  <img src="img/Java_logo.png" alt="1" />
                 </div>
                 <p>Java</p>
               </li>
               <li>
                 <div>
-                  <img src="/img/HTML_logo.png" alt="1" />
+                  <img src="img/HTML_logo.png" alt="1" />
                 </div>
                 <p>HTML</p>
               </li>
               <li>
                 <div>
-                  <img src="/img/CSS_logo.png" alt="1" />
+                  <img src="img/CSS_logo.png" alt="1" />
                 </div>
                 <p>CSS</p>
               </li>
               <li>
                 <div>
-                  <img src="/img/JavaScript_logo.png" alt="1" />
+                  <img src="img/JavaScript_logo.png" alt="1" />
                 </div>
                 <p>JavaScript</p>
               </li>
               <li>
                 <div>
-                  <img src="/img/jsp_logo.png" alt="1" />
+                  <img src="img/jsp_logo.png" alt="1" />
                 </div>
                 <p>JSP</p>
               </li>
               <li>
                 <div>
-                  <img src="/img/React_logo.png" alt="1" />
+                  <img src="img/React_logo.png" alt="1" />
                 </div>
                 <p>React</p>
               </li>
               <li>
                 <div>
-                  <img src="/img/JQeury_logo.png" alt="1" />
+                  <img src="img/JQeury_logo.png" alt="1" />
                 </div>
                 <p>JQuery</p>
               </li>
               <li>
                 <div>
-                  <img src="/img/Spring_logo.png" alt="1" />
+                  <img src="img/Spring_logo.png" alt="1" />
                 </div>
                 <p>Spring</p>
               </li>
               <li>
                 <div>
-                  <img src="/img/sql_logo.png" alt="1" />
+                  <img src="img/sql_logo.png" alt="1" />
                 </div>
                 <p>SQL</p>
               </li>
               <li>
                 <div>
-                  <img src="/img/ibatis_logo.png" alt="1" />
+                  <img src="img/ibatis_logo.png" alt="1" />
                 </div>
                 <p>ibatis</p>
               </li>
               <li>
                 <div>
-                  <img src="/img/mybatis_logo.png" alt="1" />
+                  <img src="img/mybatis_logo.png" alt="1" />
                 </div>
                 <p>mybatis</p>
               </li>
